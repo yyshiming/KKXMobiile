f4e01bac34d04792f7424fc0aeeccdce37de3dc7
ming <zhangshiming86@163.com>
2021-12-02 18:13:00 +0800

    代码优化

---
+++ Demo/Demo/Class/TextFieldController.swift
@@ -0,0 +1,29 @@
+//
+//  TextFieldController.swift
+//  Demo
+//
+//  Created by ming on 2021/12/2.
+//
+
+import UIKit
+
+class TextFieldController: KKXViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
